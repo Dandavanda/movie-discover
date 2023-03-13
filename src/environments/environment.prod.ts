0e9e2dba058a4090a52ce41583b42e4d56a10c3c
Novanda Seimahuira <38125643+Dandavanda@users.noreply.github.com>
2023-03-13 07:50:27 +0700

    Update Env Production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  image_url: "https://image.tmdb.org/t/p/w500/",
+  image_url_original: "https://image.tmdb.org/t/p/original/",
+  api_url: "https://api.themoviedb.org/3",
+  api_key: "69131ba13744341533ab45673376b2be",
+  production: false
 };
